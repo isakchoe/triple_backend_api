4d029e4e2b0a9e3f96a20c68a479d2c79dd366c2
isakchoe <isakchoe@naver.com>
2022-06-25 21:47:33 +0900

    <feature> : User 엔티티 getUser 메서드 구현
    -getUser(Long userId):  findBYId 를 메서드로 구현

--- pointApi/src/main/java/triple/pointApi/domain/user/service/UserService.java
+++ pointApi/src/main/java/triple/pointApi/domain/user/service/UserService.java
@@ -7,7 +7,6 @@ import triple.pointApi.domain.user.dto.UserPointDto;
 import triple.pointApi.domain.user.repository.UserRepository;
 import triple.pointApi.global.entity.User;
 
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,26 +14,27 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+
+    public User getUser(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NullPointerException());
+        return user;
+    }
+
     public UserPointDto getUserPoint(String account){
-
         User user = userRepository.findByAccount(account);
-
         int point = user.getPoint();
-
         UserPointDto userPointDto = new UserPointDto(account, point);
-
         return userPointDto;
     }
 
 
     public void addPoint(Long userId, int point){
-        User user = userRepository.findById(userId)
-                .orElseThrow(()-> new NullPointerException());
-
+        User user = getUser(userId);
         int prePoint = user.getPoint();
         int newPoint = prePoint + point;
-
         user.chagePoint(newPoint);
+        userRepository.save(user);
     }
 
 }
